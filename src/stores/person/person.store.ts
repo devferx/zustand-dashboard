9963ea41a74f5bce205069af4ae65f66269ec164
devferx <ferkki@hotmail.com>
2024-05-12 21:22:44 -0400

    chore: Add zustand devtools middleware

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,5 +1,5 @@
 import { create, type StateCreator } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 
 // import { customSessionStorage } from '../storages/session-storage.storage'
 import { firebaseStorage } from '../storages/firebase.storage'
@@ -14,18 +14,23 @@ interface Actions {
   setLastName: (value: string) => void
 }
 
-const storeApi: StateCreator<PersonState & Actions> = (set) => ({
+const storeApi: StateCreator<
+  PersonState & Actions,
+  [['zustand/devtools', never]]
+> = (set) => ({
   firstName: '',
   lastName: '',
 
-  setFirstName: (value) => set({ firstName: value }),
-  setLastName: (value) => set({ lastName: value }),
+  setFirstName: (value) => set({ firstName: value }, false, 'setFirstName'),
+  setLastName: (value) => set({ lastName: value }, false, 'setLastName'),
 })
 
 export const usePersonStore = create<PersonState & Actions>()(
-  persist(storeApi, {
-    name: 'person-store',
-    storage: firebaseStorage,
-    // storage: customSessionStorage,
-  }),
+  devtools(
+    persist(storeApi, {
+      name: 'person-store',
+      storage: firebaseStorage,
+      // storage: customSessionStorage,
+    }),
+  ),
 )
