cc34c4cc538a5a79a4b096e9390190edda3dd89c
devferx <ferkki@hotmail.com>
2024-05-17 12:28:36 -0400

    Add confirmation slice to wedding store

---
+++ src/stores/wedding/confirmation.slice.ts
@@ -0,0 +1,14 @@
+import { StateCreator } from 'zustand'
+
+export interface ConfirmationSlice {
+  isConfirmed: boolean
+
+  setIsConfirmed: (value: boolean) => void
+}
+
+export const createConfirmationSlice: StateCreator<ConfirmationSlice> = (
+  set,
+) => ({
+  isConfirmed: false,
+  setIsConfirmed: (value) => set({ isConfirmed: value }),
+})
